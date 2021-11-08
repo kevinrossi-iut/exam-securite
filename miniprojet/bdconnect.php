e33235cadc35c422b967edd991f8dacc27cb7f42
Kevin Rossi <rossi62u@iutm-inf-f36-23.iutmetz.site.univ-lorraine.fr>
2021-11-08 11:31:02 +0100

    fix

--- miniprojet/bdconnect.php
+++ miniprojet/bdconnect.php
@@ -1,11 +1,10 @@
 
 <?php
-$name="rossi62u_appli";
-$pass="31905588";
+$name="";
+$pass="";
 $dbname="rossi62u_securite";
 $host="devbdd.iutmetz.univ-lorraine.fr";
 try{
-    $pdo=new PDO('mysql:host=devbdd.iutmetz.univ-lorraine.fr;port=3306;dbname=rossi62u_securite','rossi62u_appli','31905588');
     $pdo=new PDO('mysql:host='.$host.';port=3306;dbname='.$dbname.'',$name , $pass);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
